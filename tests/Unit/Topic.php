3bd4a946b1be4a1400973f34d9bb033d964c6f0e
Nicholas Evans <nicholas.evans@northwestern.edu>
2018-10-30 16:37:56 -0500

    Adjust Topic tests to test w/ & w/out optional args

--- tests/Unit/Topic.php
+++ tests/Unit/Topic.php
@@ -11,18 +11,30 @@ class Topic extends TestCase
     public function test_list_all()
     {
         $response = '[{"eventHubAccount":"sysdev-test-acct","topicName":"etsysdev.test.queue.name","readTimeout":1000,"autoAcknowledge":false,"queueRealName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","alertAddress":"nicholas.evans@northwestern.edu","queueStatistics":[{"label":"QueueSize","maximum":1,"minimum":0,"startTime":"2018-10-30T14:18:00","sum":null,"current":0,"sampleSize":null,"average":0.5},{"label":"EnqueueCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null},{"label":"EnqueueTime","maximum":2076194,"minimum":5738,"startTime":"2018-10-30T14:18:00","sum":null,"current":null,"sampleSize":null,"average":784116.5967366379},{"label":"ExpiredCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null},{"label":"DispatchCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":9,"current":null,"sampleSize":null,"average":null},{"label":"DequeueCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null}],"selfPublishingAllowed":true,"destinationType":"QUEUE","name":"etsysdev.test.queue.name","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","hasMessage":false,"dlqName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ"}]';
-        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
 
+        // With period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
         $queues = $this->api->listAll(120);
+        $this->assertEquals('etsysdev.test.queue.name', $queues[0]['topicName']);
+
+        // Without period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+        $queues = $this->api->listAll();
         $this->assertEquals('etsysdev.test.queue.name', $queues[0]['topicName']);
     } // end test_list_all
 
     public function test_get_info()
     {
         $response = '{"eventHubAccount":"sysdev-test-acct","topicName":"etsysdev.test.queue.name","readTimeout":1000,"autoAcknowledge":false,"queueRealName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","alertAddress":"nicholas.evans@northwestern.edu","queueStatistics":[{"label":"QueueSize","startTime":"2018-10-30T14:22:00","maximum":1,"minimum":0,"current":0,"sum":null,"average":0.5,"sampleSize":null},{"label":"EnqueueCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null},{"label":"EnqueueTime","startTime":"2018-10-30T14:22:00","maximum":2076194,"minimum":5738,"current":null,"sum":null,"average":785780.1386910215,"sampleSize":null},{"label":"ExpiredCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null},{"label":"DispatchCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":9,"average":null,"sampleSize":null},{"label":"DequeueCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null}],"selfPublishingAllowed":true,"destinationType":"QUEUE","name":"etsysdev.test.queue.name","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","hasMessage":false,"dlqName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ"}';
-        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
 
+        // With period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
         $queue = $this->api->getInfo('etsysdev.test.queue.name', 120);
+        $this->assertEquals('etsysdev.test.queue.name', $queue['topicName']);
+
+        // Without period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+        $queue = $this->api->getInfo('etsysdev.test.queue.name');
         $this->assertEquals('etsysdev.test.queue.name', $queue['topicName']);
     } // end test_get_info
 
