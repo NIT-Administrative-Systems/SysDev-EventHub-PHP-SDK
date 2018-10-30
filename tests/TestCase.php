479660a1d21f2bde2fb8224f37398a3bf0a4a04d
Nicholas Evans <nicholas.evans@northwestern.edu>
2018-10-30 16:32:48 -0500

    Add helpers for mocking Guzzle responses & build out Topic tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,44 @@
 
 namespace Northwestern\SysDev\SOA\EventHub\Tests;
 
-/*
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Exception\RequestException;
-*/
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected $api;
+    protected $test_class;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->api = new $this->test_class('https://northwestern.edu', bin2hex(random_bytes(18)), new Client);
+    } // end setUp
+
+    protected function mockHttpResponse($status_code, $body, $headers = [])
+    {
+        $headers = array_merge(['Content-Type' => 'application/json'], $headers);
+
+        $mock = new MockHandler([
+            new Response($status_code, $headers, $body),
+        ]);
+
+        return new Client(['handler' => HandlerStack::create($mock)]);
+    } // end mockedResponse
+
+    protected function mockNetworkConnectivityError()
+    {
+        $mock = new MockHandler([
+            new RequestException('Connection timed out', new Request('GET', 'dummy')),
+        ]);
+
+        return new Client(['handler' => HandlerStack::create($mock)]);
+    } // end mockedConnError
 
 } // end TestCase
--- tests/Unit/Topic.php
+++ tests/Unit/Topic.php
@@ -6,29 +6,51 @@ use Northwestern\SysDev\SOA\EventHub\Tests\TestCase;
 
 class Topic extends TestCase
 {
+    protected $test_class = \Northwestern\SysDev\SOA\EventHub\Topic::class;
+
     public function test_list_all()
     {
-        $this->markTestIncomplete();
+        $response = '[{"eventHubAccount":"sysdev-test-acct","topicName":"etsysdev.test.queue.name","readTimeout":1000,"autoAcknowledge":false,"queueRealName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","alertAddress":"nicholas.evans@northwestern.edu","queueStatistics":[{"label":"QueueSize","maximum":1,"minimum":0,"startTime":"2018-10-30T14:18:00","sum":null,"current":0,"sampleSize":null,"average":0.5},{"label":"EnqueueCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null},{"label":"EnqueueTime","maximum":2076194,"minimum":5738,"startTime":"2018-10-30T14:18:00","sum":null,"current":null,"sampleSize":null,"average":784116.5967366379},{"label":"ExpiredCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null},{"label":"DispatchCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":9,"current":null,"sampleSize":null,"average":null},{"label":"DequeueCount","maximum":null,"minimum":null,"startTime":"2018-10-30T14:18:00","sum":1,"current":null,"sampleSize":null,"average":null}],"selfPublishingAllowed":true,"destinationType":"QUEUE","name":"etsysdev.test.queue.name","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","hasMessage":false,"dlqName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ"}]';
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+
+        $queues = $this->api->listAll(120);
+        $this->assertEquals('etsysdev.test.queue.name', $queues[0]['topicName']);
     } // end test_list_all
 
     public function test_get_info()
     {
-        $this->markTestIncomplete();
+        $response = '{"eventHubAccount":"sysdev-test-acct","topicName":"etsysdev.test.queue.name","readTimeout":1000,"autoAcknowledge":false,"queueRealName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","alertAddress":"nicholas.evans@northwestern.edu","queueStatistics":[{"label":"QueueSize","startTime":"2018-10-30T14:22:00","maximum":1,"minimum":0,"current":0,"sum":null,"average":0.5,"sampleSize":null},{"label":"EnqueueCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null},{"label":"EnqueueTime","startTime":"2018-10-30T14:22:00","maximum":2076194,"minimum":5738,"current":null,"sum":null,"average":785780.1386910215,"sampleSize":null},{"label":"ExpiredCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null},{"label":"DispatchCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":9,"average":null,"sampleSize":null},{"label":"DequeueCount","startTime":"2018-10-30T14:22:00","maximum":null,"minimum":null,"current":null,"sum":1,"average":null,"sampleSize":null}],"selfPublishingAllowed":true,"destinationType":"QUEUE","name":"etsysdev.test.queue.name","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","hasMessage":false,"dlqName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ"}';
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+
+        $queue = $this->api->getInfo('etsysdev.test.queue.name', 120);
+        $this->assertEquals('etsysdev.test.queue.name', $queue['topicName']);
     } // end test_get_info
 
     public function test_configure()
     {
-        $this->markTestIncomplete();
+        $response = '{"eventHubAccount":"sysdev-test-acct","topicName":"etsysdev.test.queue.name","readTimeout":1000,"autoAcknowledge":false,"queueRealName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","alertAddress":"nicholas.evans@northwestern.edu","queueStatistics":[],"selfPublishingAllowed":true,"destinationType":"QUEUE","name":"etsysdev.test.queue.name","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name","hasMessage":false,"dlqName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ"}';
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+
+        $config = $this->api->configure('etsysdev.test.queue.name', []);
+        $this->assertEquals('etsysdev.test.queue.name', $config['topicName']);
     } // end test_configure
 
     public function test_write_json_message()
     {
-        $this->markTestIncomplete();
+        $response_id = 'ID:12345:baz';
+        $this->api->setHttpClient($this->mockHttpResponse(204, null, ['X-message-id' => $response_id]));
+
+        $message_id = $this->api->writeJsonMessage('etsysdev.test.queue.name', ['testing' => 'messaging', 'is' => 'fun']);
+        $this->assertEquals($response_id, $message_id);
     } // end test_write_json_message
 
     public function test_write_message()
     {
-        $this->markTestIncomplete();
+        $response_id = 'ID:12345:baz';
+        $this->api->setHttpClient($this->mockHttpResponse(204, null, ['X-message-id' => $response_id]));
+
+        $message_id = $this->api->writeMessage('etsysdev.test.queue.name', '{"foo": "bar"}', 'application/json');
+        $this->assertEquals($response_id, $message_id);
     } // end test_write_message
 
 } // end Topic
