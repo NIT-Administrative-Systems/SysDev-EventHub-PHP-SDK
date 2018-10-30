f0640417a469883ed7440bd3422794007450c91a
Nicholas Evans <nicholas.evans@northwestern.edu>
2018-10-30 16:45:56 -0500

    Add Message tests

--- tests/Unit/Message.php
+++ tests/Unit/Message.php
@@ -6,29 +6,68 @@ use Northwestern\SysDev\SOA\EventHub\Tests\TestCase;
 
 class Message extends TestCase
 {
+    protected $test_class = \Northwestern\SysDev\SOA\EventHub\Message::class;
+
     public function test_read_oldest()
     {
-        $this->markTestIncomplete();
+        $response_id = 'ID:12345:baz';
+        $response_body = ['cool' => 'message'];
+
+        // Without autoAck arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, json_encode($response_body), ['X-message-id' => $response_id]));
+        $message = $this->api->readOldest('etsysdev.test.queue.name');
+        $this->assertEquals($response_id, $message->getId());
+        $this->assertEquals($response_body, $message->getMessage());
+
+        // With autoAck arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, json_encode($response_body), ['X-message-id' => $response_id]));
+        $message = $this->api->readOldest('etsysdev.test.queue.name', true);
+        $this->assertEquals($response_id, $message->getId());
+        $this->assertEquals($response_body, $message->getMessage());
+
+        // Nothing in queue
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $message = $this->api->readOldest('etsysdev.test.queue.name');
+        $this->assertNull($message);
     } // end test_read_oldest
 
     public function test_acknowledge_oldest()
     {
-        $this->markTestIncomplete();
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $status = $this->api->acknowledgeOldest('etsysdev.test.queue.name');
+        $this->assertTrue($status);
     } // end test_acknowledge_oldest
 
     public function test_read()
     {
+        // Does not currently work in EventHub
         $this->markTestIncomplete();
     } // end test_read
 
     public function test_acknowledge()
     {
-        $this->markTestIncomplete();
+        // With fastForward arg
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $status = $this->api->acknowledge('etsysdev.test.queue.name', 'ID:foobar:baz');
+        $this->assertTrue($status);
+
+        // Without fastForward arg
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $status = $this->api->acknowledge('etsysdev.test.queue.name', 'ID:foobar:baz', true);
+        $this->assertTrue($status);
     } // end test_acknowledge
 
     public function test_move()
     {
-        $this->markTestIncomplete();
+        // With delay arg
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $status = $this->api->move('etsysdev.test.queue.name', 'ID:foobar:baz', 'TOPIC', 'etsysdev.test.queue.name', 2500);
+        $this->assertTrue($status);
+
+        // Without delay arg
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+        $status = $this->api->move('etsysdev.test.queue.name', 'ID:foobar:baz', 'TOPIC', 'etsysdev.test.queue.name');
+        $this->assertTrue($status);
     } // end test_move
 
 } // end Message
