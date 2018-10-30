be4fb8324c33b975c4350c208cf1ea3e7025ebe3
Nicholas Evans <nicholas.evans@northwestern.edu>
2018-10-30 16:37:40 -0500

    Add DLQ tests

--- tests/Unit/DeadLetterQueue.php
+++ tests/Unit/DeadLetterQueue.php
@@ -6,14 +6,29 @@ use Northwestern\SysDev\SOA\EventHub\Tests\TestCase;
 
 class DeadLetterQueue extends TestCase
 {
+    protected $test_class = \Northwestern\SysDev\SOA\EventHub\DeadLetterQueue::class;
+
     public function test_get_info()
     {
-        $this->markTestIncomplete();
+        $response = '{"name":"etsysdev.test.queue.name.DLQ","realName":"Consumer.sysdev-test-acct.VirtualTopic.etsysdev.test.queue.name.DLQ","eventHubAccount":"sysdev-test-acct","destinationType":"DLQ","queueStatistics":[{"label":"QueueSize","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null},{"label":"EnqueueCount","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null},{"label":"EnqueueTime","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null},{"label":"ExpiredCount","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null},{"label":"DispatchCount","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null},{"label":"DequeueCount","maximum":null,"minimum":null,"startTime":null,"sum":null,"current":null,"sampleSize":null,"average":null}]}';
+
+        // with period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+        $dlq = $this->api->getInfo('etsysdev.test.queue.name', 120);
+        $this->assertEquals('etsysdev.test.queue.name.DLQ', $dlq['name']);
+
+        // without period arg
+        $this->api->setHttpClient($this->mockHttpResponse(200, $response));
+        $dlq = $this->api->getInfo('etsysdev.test.queue.name');
+        $this->assertEquals('etsysdev.test.queue.name.DLQ', $dlq['name']);
     } // end test_get_info
 
     public function test_move_to_dlq()
     {
-        $this->markTestIncomplete();
+        $this->api->setHttpClient($this->mockHttpResponse(204, null));
+
+        $status = $this->api->moveToDLQ('etsysdev.test.queue.name', 'ID:1234:baz');
+        $this->assertTrue($status);
     } // end test_move_to_dlq
 
 } // end DeadLetterQueue
